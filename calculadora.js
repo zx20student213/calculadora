5799af107f9473e2666f2e3b15d82c1426cff2a2
manu <zx20student213@colegiovalledelmiro.es>
2025-10-18 15:59:07 +0200

    v3: multiplicar

--- calculadora.js
+++ calculadora.js
@@ -7,6 +7,12 @@ function sumar(a, b) {
 function restar(a, b) {
   return a - b;
 }
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
 console.log("5 + 3 =", sumar(5, 3));
 console.log("5 - 3 =", restar(5, 3));
+console.log("5 * 3 =", multiplicar(5, 3));
 
